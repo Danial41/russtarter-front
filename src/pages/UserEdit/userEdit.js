62189371b512f1ef297e622a3ee3b161d61f4b88
Danial41 <prorutop@yandex.ru>
2021-06-19 17:03:27 +0300

    Successfully retreived data from datadata

--- src/pages/UserEdit/userEdit.js
+++ src/pages/UserEdit/userEdit.js
@@ -1,23 +1,30 @@
 import React, {useState} from 'react'
 
+import { URL } from "../../utils/url.helper";
+
+import { connect } from 'react-redux';
+
 const UserEditPage = (props) => {
 
+    const token = 'c63a5276874a8e4e68583996b0a2d68444d29e78';
+
+    
     const [warning, setWarning] = useState();
-
+    
     const [allValues, setAllValues] = useState({
-        first_name: '',
-        last_name: '',
-        patronymic: '',
-        birthday: '',
-        charachteristics: 'Активный поиск',
-        region_id: '',
-        snils: '',
-        egrul: '',
-        active_search: false,
-        investor: false
+      first_name: '',
+      last_name: '',
+      patronymic: '',
+      birthday: '',
+      charachteristics: 'Активный поиск',
+      region_id: '',
+      snils: '',
+      egrul: '',
+      active_search: false,
+      investor: false
     });
-
-    console.log(allValues.investor, allValues.active_search, allValues.egrul, allValues.region_id, allValues.snils);
+    
+    const query = `${allValues.snils}`
 
     const changeHandler = e => {
         setAllValues({...allValues, [e.target.name]: e.target.value})
@@ -35,6 +42,8 @@ const UserEditPage = (props) => {
     const createProjectHandler = (e) => {
         e.preventDefault()
 
+        console.log(`${allValues.last_name} ${allValues.first_name} ${allValues.patronymic}`);
+
         const formData = new FormData();
         if (allValues.first_name) formData.append('first_name', allValues.first_name);
         if (allValues.last_name) formData.append('last_name', allValues.last_name);
@@ -47,17 +56,34 @@ const UserEditPage = (props) => {
         if (allValues.investor) formData.append('investor', allValues.investor);
 
 
-        fetch(`${URL}projects`, {
-            method: 'POST',
-            body: formData
+        fetch(`${URL}/edit`, {
+            method: 'PUT',
+            body: formData,
+            headers: {
+              Authorization: 'Bearer ' + props.user.token,
+            }
         })
         .then(response => response.json())
         .then(data => {
-          if (data.error) {
+          if (data.message) {
             setWarning(data.message)
           } else {
-            props.history.push('./projects')
-            console.log(data);
+            console.log('success');
+            props.history.push('./profile')
+            if (allValues.snils) {
+              fetch('https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/party', {
+                method: 'POST',
+                mode: "cors",
+                headers: {
+                  'Content-Type': 'application/json',
+                  "Accept": 'application/json',
+                  Authorization: 'Token ' + token,
+                },
+                body: JSON.stringify({query: query})
+              })
+              .then(resp => resp.json())
+              .then(data => console.log(data))
+            }
           }
         })
         .catch(err => console.log(err));
@@ -154,12 +180,12 @@ const UserEditPage = (props) => {
                           <div>
                             <div>Характеристики</div>
                             <input type="checkbox" id="scales" name="active_search" onChange={(e) => setAllValues({...allValues, active_search: !allValues.active_search})} />
-                            <label for="scales">Активный Поиск</label>
+                            <label htmlFor="active_search">Активный Поиск</label>
                           </div>
 
                           <div>
                             <input type="checkbox" id="horns" name="investor" onChange={(e) => setAllValues({...allValues, investor: !allValues.investor})} />
-                            <label for="horns">Инвестор</label>
+                            <label htmlFor="active_search">Инвестор</label>
                           </div>
                         </div>
                         <label></label>
@@ -174,4 +200,6 @@ const UserEditPage = (props) => {
     )
 }
 
-export default UserEditPage
+const mapStateToProps = (state) => ({ user: state.user.currentUser });
+
+export default connect(mapStateToProps)(UserEditPage);
